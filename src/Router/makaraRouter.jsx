b91660812bc9a087615d11f0f24123eda49b251c
langmakara <langmakara122@gmail.com>
2025-09-17 14:23:45 +0700

    Add ExamResul page and update routing in MakaraRouter

--- src/Router/makaraRouter.jsx
+++ src/Router/makaraRouter.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../page/Home";
+import ExamResul from "../page/ExamResul";
 
 const AboutPage = () => <h2>About Page</h2>;
 const NotFoundPage = () => <h2>404 - Not Found</h2>;
@@ -9,7 +10,7 @@ const NotFoundPage = () => <h2>404 - Not Found</h2>;
 const MakaraRouter = () => (
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/about" element={<AboutPage />} />
+    <Route path="/ExamResul" element={<ExamResul />} />
     <Route path="*" element={<NotFoundPage />} />
   </Routes>
 );
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div style={{ padding: '2rem' }}>
-            <h1>Welcome to Smart Registration</h1>
-            <p>This is the home page of your application.</p>
+            <div className='card'>
+                <div className='card-body'>
+                    <h2 className='card-title'>Welcome to the University Portal</h2>
+                    <p>This is the home page of the university portal. Explore our programs and register today!</p>
+                    <div className='card-actions justify-end'>
+                        <Link to="/ExamResul" className='btn btn-primary'>Get Started</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
