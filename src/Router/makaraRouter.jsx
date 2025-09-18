650ff4ba6148cdbc6d0e8d72be0177c1360181d7
Raksa <hoeunraksa012@gmail.com>
2025-09-18 14:05:27 +0700

    ...

--- src/Router/makaraRouter.jsx
+++ src/Router/makaraRouter.jsx
@@ -2,12 +2,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ExamResul from "../page/ExamResul";
-
-
 const MakaraRouter = () => (
   <Routes>
     <Route path="/ExamResul" element={<ExamResul />} />
   </Routes>
 );
-
 export default MakaraRouter;
