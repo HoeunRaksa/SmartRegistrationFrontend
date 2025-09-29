fc8b59c98915acdf0dccec1fb32192a7d361c35d
Raksa <hoeunraksa012@gmail.com>
2025-09-29 15:08:27 +0700

    ....

--- src/Components/Nabar.jsx
+++ src/Components/Nabar.jsx
@@ -4,10 +4,10 @@ import "../App.css"
 
 function Navbar() {
   return (
-    <nav className="backdrop-blur-md glass border border-gray-200 rounded shadow-md my-2">
-      <div className="flex items-center justify-between px-6 py-4">
+    <nav className="backdrop-blur-md glass border border-gray-200 rounded shadow-md my-1">
+      <div className="flex items-center justify-between px-6 py-6">
         {/* Logo/Brand */}
-        <div className="text-white font-bold text-xl tracking-wide">
+        <div className="text-gray-700 font-bold text-xl tracking-wide">
           Department Of War
         </div>
 
@@ -18,9 +18,9 @@ function Navbar() {
             className="flex items-center space-x-2 text-gray-700 hover:text-blue-600 transition-all duration-200"
           >
             <div className="w-9 h-9 bg-green-500 rounded-lg flex items-center justify-center shadow hover:scale-110 transition">
-              <Home className="w-4 h-4 text-white" />
+              <Home className="w-4 h-4 text-gray-700" />
             </div>
-            <span className="text-sm text-white khmerFont">ទំព័រដើម</span>
+            <span className="text-sm text-gray-700 khmerFont">ទំព័រដើម</span>
           </a>
 
           <a
@@ -28,9 +28,9 @@ function Navbar() {
             className="flex items-center space-x-2 text-gray-700 hover:text-blue-600 transition-all duration-200"
           >
             <div className="w-9 h-9 bg-blue-500 rounded-lg flex items-center justify-center shadow hover:scale-110 transition">
-              <FileText className="w-4 h-4 text-white" />
+              <FileText className="w-4 h-4 text-gray-700" />
             </div>
-            <span className="text-sm text-white khmerFont">ការចុះឈ្មោះ</span>
+            <span className="text-sm text-gray-700 khmerFont">ការចុះឈ្មោះ</span>
           </a>
 
           <a
@@ -38,9 +38,9 @@ function Navbar() {
             className="flex items-center space-x-2 text-gray-700 hover:text-blue-600 transition-all duration-200"
           >
             <div className="w-9 h-9 bg-gradient-to-r from-orange-400 to-red-500 rounded-lg flex items-center justify-center shadow hover:scale-110 transition">
-              <BarChart3 className="w-4 h-4 text-white" />
+              <BarChart3 className="w-4 h-4 text-gray-700" />
             </div>
-            <span className="text-sm text-white khmerFont">កម្មវិធីសិក្សា</span>
+            <span className="text-sm text-gray-700 khmerFont">កម្មវិធីសិក្សា</span>
           </a>
 
           <a
@@ -48,12 +48,12 @@ function Navbar() {
             className="flex items-center space-x-2 text-gray-700 hover:text-blue-600 transition-all duration-200"
           >
             <div className="w-9 h-9 bg-cyan-500 rounded-lg flex items-center justify-center shadow hover:scale-110 transition">
-              <Info className="w-4 h-4 text-white" />
+              <Info className="w-4 h-4 text-gray-700" />
             </div>
-            <span className="text-sm text-white khmerFont">អំពីយើង</span>
+            <span className="text-sm text-gray-700 khmerFont">អំពីយើង</span>
           </a>
           {/* Login Button */}
-          <Button className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white px-6 py-2 rounded-xl font-semibold shadow-md hover:shadow-lg transition-all">
+          <Button className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-gray-700 px-6 py-2 rounded-xl font-semibold shadow-md hover:shadow-lg transition-all">
             Login
           </Button>
         </div>
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,9 @@
 import BackGrpundImg from "../assets/Images/Background.png";
 const Home = () => {
     return (
-        <section className="w-full rounded bg-gray-100 relative overflow-hidden bodyglass">
-          <div  className="w-full sm:p-3 p-1 sm:text-5xl text-3xl font-bold"> 
-            Welcome to <span className="text-blue-500">University</span> Registration System
+        <section className="w-full rounded bg-gray-100 relative overflow-hidden">
+           <div  className="w-full "> 
+            <p className="sm:p-3 p-1 font-bold sm:text-5xl text-2xl text-center text-gray-700">Welcome to <span className="text-blue-500">University</span> Registration</p>
            </div>
         </section>
     );
