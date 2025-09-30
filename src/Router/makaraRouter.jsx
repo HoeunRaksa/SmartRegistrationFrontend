40a7a939394bd8b348373c50d3a4ceeb28fc86f3
langmakara <langmakara122@gmail.com>
2025-09-30 18:21:14 +0700

    Page login

--- src/Router/makaraRouter.jsx
+++ src/Router/makaraRouter.jsx
@@ -1,13 +1,15 @@
-// makaraRouter.js
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ExamResul from "../page/ExamResul";
 import AbouteUs from "../page/AbouteUs";
+import Login from "../page/Login";
 
 const MakaraRouter = () => (
   <Routes>
-    <Route path="/ExamResul" element={<ExamResul />} />
-    <Route path="/AbouteUs" element={<AbouteUs />} />
+    <Route path="/examresul" element={<ExamResul />} />
+    <Route path="/aboutus" element={<AbouteUs />} />
+    <Route path="/login" element={<Login />} />
   </Routes>
 );
+
 export default MakaraRouter;
