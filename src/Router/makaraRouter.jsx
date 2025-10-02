494983e990e46bfa9f5fd93d48e2932559ce1873
langmakara <langmakara122@gmail.com>
2025-10-02 15:47:07 +0700

    New page Notification

--- src/Router/makaraRouter.jsx
+++ src/Router/makaraRouter.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import ExamResul from "../page/ExamResul";
 import AbouteUs from "../page/AbouteUs";
 import Login from "../page/Login";
+import NotificationInbox from "../page/Notification";
 
 const MakaraRouter = () => (
   <Routes>
     <Route path="/examresul" element={<ExamResul />} />
     <Route path="/aboutus" element={<AbouteUs />} />
     <Route path="/login" element={<Login />} />
+    <Route path="/notification" element={<NotificationInbox />} />
   </Routes>
 );
 
