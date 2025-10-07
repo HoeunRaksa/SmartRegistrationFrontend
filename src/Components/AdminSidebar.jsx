5871017fa110304e619015c5cbb36f23a743b0c9
MonyKhmer <monykh1074@gmail.com>
2025-10-07 22:33:05 +0700

    Mony

--- src/Components/AdminSidebar.jsx
+++ src/Components/AdminSidebar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { ChevronFirst, LayoutDashboard, DollarSign, FileText, Settings, GraduationCap, CreditCard } from "lucide-react"
+import { ChevronFirst, LayoutDashboard, DollarSign, FileText, Settings, GraduationCap, CreditCard, UserIcon } from "lucide-react"
 import Profile from "../assets/Images/profile.png"
 import Logo from "../assets/Images/AdminLogo.png"
 import PaidStudentsTable from "./PaidStudentsTable"
@@ -9,6 +9,7 @@ const navItems = [
   { title: "Dashboard", icon: LayoutDashboard, id: "dashboard" },
   { title: "Paid Students", icon: CreditCard, id: "paid-students" },
   { title: "All Students", icon: GraduationCap, id: "all-students" },
+  { title: "Manage Students", icon: UserIcon, id: "manage-students" },
   { title: "Payments", icon: DollarSign, id: "payments" },
   { title: "Reports", icon: FileText, id: "reports" },
   { title: "Settings", icon: Settings, id: "settings" },
@@ -25,6 +26,8 @@ const AdminSidebar = ({ children }) => {
         return <div className="text-gray-600">Dashboard Content</div>
       case "all-students":
         return <div className="text-gray-600">All Students Content</div>
+      case "manage-students":
+        return <div className="text-gray-600">Manage Students Content</div>
       case "payments":
         return <div className="text-gray-600">Payments Content</div>
       case "reports":
