4bd7dbf289bc5de827a6aa7810eaa80e1ad5c3fd
HoeunRaksa <hoeunraksa12@gmail.com>
2026-02-01 22:35:35 +0700

    Removed padding from PaymentPage to match Admin page style

--- src/studentSide/StudentPage/PaymentPage.jsx
+++ src/studentSide/StudentPage/PaymentPage.jsx
@@ -183,7 +183,7 @@ const PaymentPage = () => {
     }
 
     return (
-        <div className="p-6 space-y-6">
+        <div className="space-y-6">
             {/* Header section with Stats */}
             <motion.div
                 variants={containerVariants}
